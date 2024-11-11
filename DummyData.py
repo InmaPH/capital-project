596ea1be65ba587f0fb2b9b42648ef843ce9c5cf
InmaPH <inmaperrottihuertas@outlook.com>
2024-11-11 16:21:31 +0000

    Script for creating dummy data for future analysis

---
+++ DummyData.py
@@ -0,0 +1,47 @@
+#Import the module
+from faker import Faker
+import pandas as pd
+
+# Initialize the Faker object and set up a seed 
+
+fake = Faker()
+Faker.seed(13)
+
+# Dictionary with 100 entries of data
+## !!! Note that "Email" and "First/Last Name" needs fix. 
+## !!! Currently displays different first/last names that do not correlate with email
+
+def generate_fake_data(num_entries=100):
+    data={
+        'First Name':[],
+        'Last Name':[],
+        'Email':[],
+        'Phone Number':[],
+        'Location':[],
+        'Profession':[],
+        'Company':[],
+        'Date':[]
+    }
+
+    for _ in range(num_entries):
+        data['First Name'].append(fake.first_name())
+        data['Last Name'].append(fake.last_name())
+        data['Email'].append(f"{fake.first_name()}.{fake.last_name()}@{fake.free_email_domain()}")
+        data['Phone Number'].append(fake.phone_number())
+        data['Location'].append(fake.country())
+        data['Profession'].append(fake.job())
+        data['Company'].append(fake.company())
+        data['Date'].append(fake.date_between(start_date='-1y', end_date='+1y'))
+
+    return data
+    
+#Generate the data
+dummy_data = generate_fake_data(100)
+
+#Create a DataFrame
+df_dummy_data = pd.DataFrame(dummy_data)
+
+#Save to a CSV file
+df_dummy_data.to_csv('dummy_data.csv', index=False)
+
+print(df_dummy_data.head())
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # capital-project
 Using dummy data, this project aims to analyse hardware spending.
+
+Packages used: Faker
+
+1) Create dataset.
+Usually in a business setting this data is available for extraction and transformation. However, since this project is just a showcase, I'll be using dummy data that would be similar to what companies have available in their FinOps environment.
+
+Faker is a Python library that allows us to create random data points that can be stored into a dataset. 
+For this particular case, I'll be using:
+
+       - 'First Name'
+       - 'Last Name'
+       - 'Email'
+       - 'Phone Number'
+       - 'Location'
+       - 'Profession'
+       - 'Company'
+       - 'Date'
